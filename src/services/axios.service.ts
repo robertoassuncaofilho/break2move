8302bbf37d367f0af88cc2c6bb6917f69fddf099
Roberto Buarque <roberto.16@gmail.com>
2021-03-15 17:39:04 -0300

    backend https

--- src/services/axios.service.ts
+++ src/services/axios.service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8000/':'http://break2move.herokuapp.com',
+baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8000/':'https://break2move.herokuapp.com',
 timeout: 3000
 });
